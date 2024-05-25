00e9e4b92e069cd51e615f3c00c6f739176a750a
cirocavalleggeri <ciroallinone@gmail.com>
2024-05-25 20:43:52 +0200

    Progetto finale
    Il Progetto di Denis Panjuta è finito.In questo progetto si sono trattati i menu a scomparsa ,lo stato delle variabili mutevoli ricordate dalla parola chiave remember ,l'applicazione della parola by ad un oggetto che permette di trattare l'oggetto come una semplicde variabile var,e infine la creazione di uno stle personalizzato con TextStyle da applicare a un oggetto Text()

--- app/src/main/java/com/example/unicoverter/MainActivity.kt
+++ app/src/main/java/com/example/unicoverter/MainActivity.kt
@@ -32,10 +32,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.unicoverter.ui.theme.UniCoverterTheme
+import java.time.format.TextStyle
 import kotlin.math.roundToInt
 
 
@@ -59,26 +63,34 @@ class MainActivity : ComponentActivity() {
 fun UnitConveter(){
     var inputValue by  remember { mutableStateOf("") }
     var outputValue by  remember { mutableStateOf("") }
-    var inputUnnit by  remember { mutableStateOf("Centimetri") }
-    var outputtValue by  remember { mutableStateOf("Metri") }
+    var inputUnnit by  remember { mutableStateOf("Metri") }
+    var outputUnit by  remember { mutableStateOf("Metri") }
     var iExplanded by  remember { mutableStateOf(false) }
     var oExplanded by  remember { mutableStateOf(false) }
-    val conversionFactor =  remember { mutableStateOf(0.01) }
+    val conversionFactor =  remember { mutableStateOf(1.0) }
+    val oconversionFactor =  remember { mutableStateOf(1.0) }
+    val customStyle= androidx.compose.ui.text.TextStyle(
+        fontFamily = FontFamily.Monospace,
+        color = Color.Red,
+        fontSize = 32.sp
+
+    )
     fun convertUnit(){
         val inputValueDouble=inputValue.toDoubleOrNull() ?:0.0
-        val risultato = (conversionFactor.value*inputValueDouble*100).roundToInt()/100
+        val risultato = ((conversionFactor.value*inputValueDouble*100/oconversionFactor.value))/100
+       //outputValue=String.format("%.2f",risultato)
         outputValue=risultato.toString()
-
     }
     Column (modifier = Modifier.fillMaxSize()
             , verticalArrangement = Arrangement.Center
         , horizontalAlignment = Alignment.CenterHorizontally
     ){
-        Text(text = "Unit Converter")
+        Text(text = "Unit Converter", style = customStyle)
         Spacer(modifier=Modifier.height(16.dp))
         OutlinedTextField(
             value = inputValue,
-            onValueChange = {inputValue=it}
+            onValueChange = {inputValue=it
+                             convertUnit()}
             ,label = {Text("Enter value")
             }
         )
@@ -91,7 +103,7 @@ fun UnitConveter(){
                 val context= LocalContext.current
                 Button(onClick = {Toast.makeText(context,"Bottone premuto",Toast.LENGTH_LONG).show()
                                   iExplanded=!iExplanded}) {
-                    Text(text = "Seleziona")
+                    Text(text = inputUnnit)
                     Icon(Icons.Default.ArrowDropDown, contentDescription ="" )
 
                 }
@@ -117,7 +129,7 @@ fun UnitConveter(){
                     DropdownMenuItem(text = { Text(text = "Millimetri") },
                         onClick = {iExplanded=!iExplanded
                             inputUnnit="Millimetri"
-                            conversionFactor.value=1000.0
+                            conversionFactor.value=0.001
                             convertUnit() })
                 }
             }
@@ -131,23 +143,40 @@ fun UnitConveter(){
                                     }
 
                                     ) {
-                    Text(text = "Seleziona")
+                    Text(text = outputUnit)
                     Icon(Icons.Default.ArrowDropDown, contentDescription ="" )
                 }
                 DropdownMenu(expanded =  oExplanded, onDismissRequest = { oExplanded=false }) {
                     DropdownMenuItem(text = { Text(text = "Centimetri") },
-                        onClick = { oExplanded=false })
+                        onClick = { oExplanded=false
+                            outputUnit="Centimetri"
+                            oconversionFactor.value=0.01
+                            convertUnit() })
                     DropdownMenuItem(text = { Text(text = "Metri") },
-                        onClick = { oExplanded=false })
+                        onClick = { oExplanded=false
+                            outputUnit="Metri"
+                            oconversionFactor.value=1.00
+                            convertUnit()
+                        })
                     DropdownMenuItem(text = { Text(text = "Piedi") },
-                        onClick = { oExplanded=false })
+                        onClick = { oExplanded=false
+                            outputUnit="Piedi"
+                            oconversionFactor.value=0.3048
+                            convertUnit()
+                        })
                     DropdownMenuItem(text = { Text(text = "Millimetri") },
-                        onClick = {oExplanded=false})
+                        onClick = {oExplanded=false
+                            outputUnit= "Millimetri"
+                            oconversionFactor.value=0.001
+                            convertUnit()
+
+                        })
                 }
             }
         }
         Spacer(modifier=Modifier.height(16.dp))
-        Text(text = "Risultato: $outputValue",modifier= Modifier.padding(top = 32.dp))
+        Text(text = "Risultato: $outputValue $outputUnit",modifier= Modifier.padding(top = 32.dp),
+            style = MaterialTheme.typography.headlineMedium)
     }
 }
 
